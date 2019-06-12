0d9d8008eb88ef8bcd939ebb6cdc130cd1bbcbbd
Erick Pacheco Pedraza <eum602@gmail.com>
2019-06-12 15:59:45 -0500

    basic example how to use float and flex to handle css elements

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -8,9 +8,16 @@ const layout = props =>(//Layout for the page
     <Aux>
         <Toolbar/>
         <SideDrawer/>
-        <main className={classes.Content}>
+        <main className={classes.Content}> {/* adding margin with respect to its parent; all
+        wrapper and components done in react do not add style in the project; we add style directly in
+        html elements */}
             {props.children}
         </main>
+        <div className={classes.Parent} >
+            <div className={classes.Son}>p1</div>
+            <p className={classes.Son}>p2</p>
+            <p className={classes.Son3}>p3</p>
+        </div>
     </Aux>
 )
 
