f559af3817e9bdfce6c324cd7d9efcb05a5b60c3
Erick Pacheco Pedraza <eum602@gmail.com>
2019-06-13 17:26:18 -0500

    built logo for toolbar stateless component

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import burgerLogo from "../../assets/burger-logo.png"
+import burgerLogo from "../../assets/burger-logo.png" //importing logo
 import classes from './Logo.css'
 const logo = props => (
     <div className={classes.Logo}>
-        <img src={burgerLogo} alt="MyBurger"/>
+        <img src={burgerLogo} alt="MyBurger"/> {/*Getting the logo from elsewhere Webpack put that 
+        when rendered on the client side*/}
     </div>
 )
 export default logo
