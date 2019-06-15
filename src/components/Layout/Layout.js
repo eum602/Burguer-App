5294434f184a1e3bde42ba85f11398dd767f00a1
Erick Pacheco Pedraza <eum602@gmail.com>
2019-06-14 19:18:30 -0500

    added a BACKDROP to a sidebar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,19 +1,30 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Aux from "../../hoc/Aux"
 import classes from "./Layout.css"
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = props =>(//Layout for the page
-    <Aux>
-        <Toolbar/>
-        <SideDrawer/>
-        <main className={classes.Content}> {/* adding margin with respect to its parent; all
-        wrapper and components done in react do not add style in the project; we add style directly in
-        html elements */}
-            {props.children}
-        </main>        
-    </Aux>
-)
+class Layout extends Component {
+    state = {
+        showSideDrawer:true
+    }
+    SideDrawerCloseHanlder = () => {
+        this.setState({showSideDrawer:false})
+    }
+    render(){
+        return(
+            <Aux>
+                <Toolbar/>
+                <SideDrawer open={this.state.showSideDrawer} closed={this.SideDrawerCloseHanlder} />
+                <main className={classes.Content}> {/* adding margin with respect to its parent; all
+                wrapper and components done in react do not add style in the project; we add style directly in
+                html elements */}
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
 
-export default layout
+    }
+}
+
+export default Layout
