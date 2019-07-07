9073eadd2d7a1145fe4fd8d309a148bb3e39a38d
Erick Pacheco Pedraza <eum602@gmail.com>
2019-07-06 23:02:30 -0500

    added contactData form in a NESTED route

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 import CheckoutSummary from'../../components/Order/CheckoutSummary/CheckoutSummary'
+import {Route} from 'react-router-dom'
+import ContactData from './ContactData/ContactData'
 class Checkout extends Component{
     state={
         ingredients:{
@@ -35,7 +37,9 @@ class Checkout extends Component{
                     ingredients={this.state.ingredients}
                     checkoutCancelled= {this.checkoutCancelledHandler}
                     checkoutContinued={this.checkoutContinuedHandler}
-                    />
+                />
+                <Route path={`${this.props.match.path}/contact-data`} component={ContactData}/> {/*Using this nested route to be executed 
+                in sychorization with checkoutContinuedHandler */}
             </div>
         )
     }    
