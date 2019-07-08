c7d4393993a5796704bb311df19a2b542e48db8c
Erick Pacheco Pedraza <eum602@gmail.com>
2019-07-07 21:57:34 -0500

    passed 'Exact' as a prop to NavLink so that we control EXACT in an AUTOMATED WAY - l256

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -6,7 +6,9 @@ const navigationItem = props =>(
         <li>
             <NavLink
                 to={props.link}
-                exact /**Adding "EXACT" keyword so that active class is only attached
+                exact={props.exact} /**receiving exact prop from parent so that
+                it now can be false or true and thus exact is set in an automatic way                
+                Adding "EXACT" keyword so that active class is only attached
                 when URL path matches EXACTLY the "to"*/
                 activeClassName={classes.active}/**Assigning a custom className
                 otherwise react-router-dom will assign an automatic class=active(but
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,8 +3,9 @@ import classes from './NavigationItems.css'
 import NavigationItem from './NavigationItem/NavigationItem';
 const navigationItems = props =>(//active es boolean, por eso se pasa asi.
     <ul className={classes.NavigationItems}>
-        <NavigationItem link="/" >Burger Builder</NavigationItem>{/**
-        active is set automatically due to NavLink(react-router-dom) on NavigationItem component*/}
+        <NavigationItem link="/" exact>Burger Builder</NavigationItem>{/**
+        Using "EXACT" keyword to pass to NavigationItem as a prop, so that we
+        can control wich NavLink in NavigationItem component receives exact or not*/}
         <NavigationItem link="/orders">Orders</NavigationItem>
     </ul>
 )
