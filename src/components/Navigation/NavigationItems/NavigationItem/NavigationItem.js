d6a40052acab4460b076740d0c32aa6d02d662e6
Erick Pacheco Pedraza <eum602@gmail.com>
2019-07-07 21:13:48 -0500

    added custom ACTIVECLASSNAME to attach specific style when using NavLink(react-router-dom) and so avoid default active class name and its troubles with css styles - l256

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import {NavLink}  from 'react-router-dom'
 import classes from './NavigationItem.css'
 const navigationItem = props =>(
     <ul className={classes.NavigationItem}>
         <li>
-            <a 
-                href={props.link}
-                className={props.active ? classes.active:null}>
+            <NavLink
+                to={props.link}                
+                activeClassName={classes.active}/**Assigning a custom className
+                otherwise react-router-dom will assign an automatic class=active(but
+                as React assigns unique class names for all then it wont match
+                the active class for an anchor defined in NavivationItem.CSS) */
+            >
                 {props.children}
-            </a>
+            </NavLink>
         </li>
     </ul>
 )
