22b0278e7c35f19f24113c10b3feda8b4a7fe9bd
Erick Pacheco Pedraza <eum602@gmail.com>
2019-07-07 21:19:41 -0500

    added EXACT keyword on NavLink(react-router-dom) so that, in a similar way to Route, only url path that exactly matches with 'to' in NavLink will trigger its default or custom active class for each NavLink

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -5,7 +5,9 @@ const navigationItem = props =>(
     <ul className={classes.NavigationItem}>
         <li>
             <NavLink
-                to={props.link}                
+                to={props.link}
+                exact /**Adding "EXACT" keyword so that active class is only attached
+                when URL path matches EXACTLY the "to"*/
                 activeClassName={classes.active}/**Assigning a custom className
                 otherwise react-router-dom will assign an automatic class=active(but
                 as React assigns unique class names for all then it wont match
